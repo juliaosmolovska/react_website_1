13dc537ac554718a672aacf01217ca76af89a0bb
juliaosmolovska <juliaosmolovska@gmail.com>
2021-01-29 15:56:05 +0200

    4

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button } from './Button';
 import { Link } from 'react-router-dom';
+import './Navbar.css'
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -10,15 +12,23 @@ function Navbar() {
   
   const showButton = () => {
     if(window.innerWidth <= 960)  {
-      setButton(false)
+      setButton(false);
+    } else {
+      setButton(true);
     }
-  }
+  };
+  
+  useEffect( () => {
+    showButton()
+  }, []);
+  
+  window.addEventListener('resize', showButton);
   
   return (
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <Link to='/' className='navbar-logo'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
             TRVL <i className='fab fa-typo3'></i>
           </Link>
           <div className='menu-icon' onClick={handClick}>
