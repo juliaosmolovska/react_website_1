e7cc755bfe75edd3187afca25a47eff5e6ac7ddc
juliaosmolovska <juliaosmolovska@gmail.com>
2021-02-02 15:31:02 +0200

    Add Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section className='footer-subscription'>
+        <p className='footer-subscription-heading'>
+          Join the Adventure newsletter to receive our best vacation deals
+        </p>
+        <p className='footer-subscription-text'>
+          You can unsubscribe at any time.
+        </p>
+        <div className='input-areas'>
+          <form>
+            <input type='email' name='email' placeholder='Your Email' className='footer-input'/>
+              <Button buttonStyle='btn--outline'>
+                Subscribe
+              </Button>
+          </form>
+        </div>
+      </section>
+      <div className='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div className='footer-link-items'>
+            <h2>About Us</h2>
+            <Link>How it works</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
